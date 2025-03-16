7ce0964306906e7b6aa4457a55d3195ccc88c29e
Aleksandra Kovaleva <akovaleva1@sheffield.ac.uk>
2025-03-16 20:04:49 +0000

    feat: exercise 2, implementing fxtradeloader

--- c-sharp/Loaders/FxTradeLoader.cs
+++ c-sharp/Loaders/FxTradeLoader.cs
@@ -4,21 +4,55 @@ namespace HmxLabs.TechTest.Loaders
 {
     public class FxTradeLoader : ITradeLoader
     {
-        // NOTE: These methods are only here to allow the solution to compile prior to the test being completed.
+        private const char Seperator = '¬';
+        
         public IEnumerable<ITrade> LoadTrades()
         {
-            throw new NotImplementedException();
+            var tradeList = new FxTradeList();
+            LoadTradesFromFile(DataFile, tradeList);
+            return tradeList;
         }
 
-        public string? DataFile
+        public string? DataFile { get; set; }
+
+        private FxTrade CreateTradeFromLine(string line_)
         {
-            get
+            
+            var items = line_.Split(new[] {Seperator});
+            var trade = new FxTrade(items[8], items[0], DateTime.Parse(items[6]));
+            trade.TradeDate = DateTime.Parse(items[1]);
+            trade.Instrument = items[2] + items[3];
+            trade.Notional = Double.Parse(items[4]);
+            trade.Rate = Double.Parse(items[5]);
+            trade.Counterparty = items[7];
+            
+            return trade;
+        }
+
+        private void LoadTradesFromFile(string? filename_, FxTradeList tradeList_)
+        {
+            if (null == filename_)
+                throw new ArgumentNullException(nameof(filename_));
+            
+            var stream = new StreamReader(filename_);
+
+            using (stream)
             {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
+                var lineCount = 0;
+                while (!stream.EndOfStream)
+                {
+                    var line = stream.ReadLine();
+                    if (line == null)
+                        continue;
+
+                    if (line.StartsWith("END"))
+                        break;
+
+                    if (lineCount >= 2) {
+                        tradeList_.Add(CreateTradeFromLine(line));
+                    }
+                    lineCount++;
+                }
             }
         }
     }
--- c-sharp/Models/FxTrade.cs
+++ c-sharp/Models/FxTrade.cs
@@ -4,13 +4,29 @@ namespace HmxLabs.TechTest.Models
 {
     public class FxTrade : BaseTrade
     {
+        public FxTrade(string tradeId_, string tradeType_, DateTime valueDate_)
+        {
+            if (string.IsNullOrWhiteSpace(tradeId_))
+            {
+                throw new ArgumentException("A valid non null, non empty trade ID must be provided");
+            }
+            if (string.IsNullOrWhiteSpace(tradeType_))
+            {
+                throw new ArgumentException("A valid non null, non empty trade type must be provided");
+            }
+            if (valueDate_ == default(DateTime))
+            {
+                throw new ArgumentException("A valid value date must be provided");
+            }
+            
+            TradeId = tradeId_;
+            TradeType = tradeType_;
+            ValueDate = valueDate_;
+        }
         public const string FxSpotTradeType = "FxSpot";
         public const string FxForwardTradeType = "FxFwd";
 
-        public override string TradeType
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public override string TradeType { get; }
 
         public DateTime ValueDate { get; set; }
     }
