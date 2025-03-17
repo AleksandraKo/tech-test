f33f038179827d404442cb3afab624088b9bf26d
Aleksandra Kovaleva <akovaleva1@sheffield.ac.uk>
2025-03-17 08:48:21 +0000

    feat: exercise 4, implementing the loader, updating file references

--- c-sharp/RiskSystem/SerialPricer.cs
+++ c-sharp/RiskSystem/SerialPricer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using HmxLabs.TechTest.Models;
 
@@ -28,12 +29,48 @@ namespace HmxLabs.TechTest.RiskSystem
 
         private void LoadPricers()
         {
-            var pricingConfigLoader = new PricingConfigLoader { ConfigFile = @".\PricingConfig\PricingEngines.xml" };
+            string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PricingConfig", "PricingEngines.xml");
+            var pricingConfigLoader = new PricingConfigLoader { ConfigFile = configPath };
             var pricerConfig = pricingConfigLoader.LoadConfig();
 
             foreach (var configItem in pricerConfig)
             {
-                throw new NotImplementedException();
+                string assemblyName = configItem.Assembly ?? throw new InvalidOperationException("Pricing engine assembly name not provided");
+                string pricerType = configItem.TypeName ?? throw new InvalidOperationException("Pricing engine type name not provided");
+                string tradeType = configItem.TradeType ?? throw new InvalidOperationException("Trade type not provided");
+
+                try
+                {
+                    string assemblyPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Pricers", "Pricers.dll");
+                    assemblyPath = Path.GetFullPath(assemblyPath);
+                    // Console.WriteLine($"Loading assembly from: {assemblyPath}");
+                    // if (!File.Exists(assemblyPath))
+                    // {
+                    //     Console.WriteLine($"Error: Assembly file {assemblyPath} not found!");
+                    //     continue;
+                    // }
+                    
+                    Assembly pricersAssembly = Assembly.LoadFrom(assemblyPath); // Load the assembly dynamically
+                    Type? pricerClass = pricersAssembly.GetType(pricerType); //reflection to dynamically load the pricer class type
+                    if (pricerClass == null)
+                    {
+                        throw new InvalidOperationException($"Pricing engine class {pricerType} could not be found.");
+                    }
+
+                    // Create an instance of the pricing engine
+                    var pricerInstance = Activator.CreateInstance(pricerClass) as IPricingEngine;
+                    if (pricerInstance == null)
+                    {
+                        throw new InvalidOperationException($"Failed to create an instance of {pricerType}.");
+                    }
+                    
+                    _pricers[tradeType] = pricerInstance;
+                    // Console.WriteLine($"Successfully loaded pricer for {tradeType}: {pricerType}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error loading pricing engine for {tradeType}: {ex.Message}");
+                }
             }
         }
 
--- c-sharp/RiskSystem/SerialTradeLoader.cs
+++ c-sharp/RiskSystem/SerialTradeLoader.cs
@@ -12,15 +12,25 @@ namespace HmxLabs.TechTest.RiskSystem
             return loaders.Select(loader => loader.LoadTrades()).ToList();
         }
 
+        // private IEnumerable<ITradeLoader> GetTradeLoaders()
+        // {
+        //     var loaders = new List<ITradeLoader>();
+        //     ITradeLoader loader = new BondTradeLoader {DataFile = @"TradeData/BondTrades.dat"};
+        //     loaders.Add(loader);
+
+        //     loader = new FxTradeLoader { DataFile = @"TradeData/FxTrades.dat" };
+        //     loaders.Add(loader);
+            
+        //     return loaders;
+        // }
         private IEnumerable<ITradeLoader> GetTradeLoaders()
         {
-            var loaders = new List<ITradeLoader>();
-            ITradeLoader loader = new BondTradeLoader {DataFile = @"TradeData/BondTrades.dat"};
-            loaders.Add(loader);
+            string tradeDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TradeData");
 
-            loader = new FxTradeLoader { DataFile = @"TradeData/FxTrades.dat" };
-            loaders.Add(loader);
-            
+            var loaders = new List<ITradeLoader>();
+            loaders.Add(new BondTradeLoader { DataFile = Path.Combine(tradeDataPath, "BondTrades.dat") });
+            loaders.Add(new FxTradeLoader { DataFile = Path.Combine(tradeDataPath, "FxTrades.dat") });
+
             return loaders;
         }
     }
